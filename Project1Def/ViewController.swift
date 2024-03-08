25c00dd0e3cb56a04046e97d4ff5375e0e5b4fa8
juan.roncancio <sbm029@gmail.com>
2024-03-07 19:59:18 -0500

    creating repo

--- Project1Def/ViewController.swift
+++ Project1Def/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var copies = ["Primer texto", "Segunto texto", "Tercer Texto","Cuarto Texto", "Quinto Texto"]
+    var currentIndex = 0
+    var currentIndex2 = 0
+    
+    @IBOutlet weak var imageToChange: UIImageView!
+    
+    
+    @IBOutlet weak var textToChange: UILabel!
+    
+    
+    @IBOutlet weak var buttonToChangeText: UIButton!
+    
+    
+    @IBOutlet weak var buttonToChangeColorFont: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        buttonToChangeText.setTitle("Cambiar Motocicleta", for: .normal)
+        buttonToChangeColorFont.setTitle("Cambiar color de texto", for: .normal)
     }
 
-
+    @IBAction func changeText(_ sender: Any) {
+        if currentIndex < copies.count{
+            textToChange.text = copies[currentIndex]
+            currentIndex += 1
+        } else {
+            currentIndex = 0
+        }
+    }
+    
+    @IBAction func changeColorFont(_ sender: Any) {
+        
+        if currentIndex < copies.count{
+            textToChange.textColor = .purple
+            currentIndex2 += 1
+        } else {
+            currentIndex2 = 0
+        }
+        
+    }
 }
 
